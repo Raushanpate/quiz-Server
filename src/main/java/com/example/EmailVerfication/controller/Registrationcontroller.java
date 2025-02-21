8f4add165a1cb49c512f2bce6875b857586be6c1
Raushanpate <133678807+Raushanpate@users.noreply.github.com>
2025-02-21 09:38:13 +0530

    Update Registrationcontroller.java

--- src/main/java/com/example/EmailVerfication/controller/Registrationcontroller.java
+++ src/main/java/com/example/EmailVerfication/controller/Registrationcontroller.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = { "*"})
+@CrossOrigin(origins = "https://123raushan.github.io")
 @RequestMapping("/apiEmail")
 public class Registrationcontroller {
 
