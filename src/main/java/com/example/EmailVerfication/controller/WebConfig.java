8c8ba812424fc1208e6c15ab70e6d29247886eba
Raushanpate <133678807+Raushanpate@users.noreply.github.com>
2025-02-22 11:33:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/EmailVerfication/controller/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.EmailVerfication.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // Change "*" to frontend URL for security
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
